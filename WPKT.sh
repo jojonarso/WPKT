9d32c43ef3cbcc5563c9f0727b98d6bbb8ebd4ed
jojonarso <92215972+jojonarso@users.noreply.github.com>
2021-10-10 07:14:57 +0700

    Create WPKT.sh

---
+++ WPKT.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 &&./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q82ywzvjwtrcne84svvc8r3wyrvntn3vmd9k45a http://pool.srizbi.com
